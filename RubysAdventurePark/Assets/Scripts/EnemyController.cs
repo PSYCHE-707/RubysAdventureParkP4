18c6adaad9c4584255f7f2d5fc16e6946ab38e47
PSYCHE-707 <michpark200519@gmail.com>
2022-12-02 11:45:02 -0800

    Visual Styling
    Finished particle effects and cinemachine. Started UI Canvas.

--- RubysAdventurePark/Assets/Scripts/EnemyController.cs
+++ RubysAdventurePark/Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
@@ -85,6 +87,8 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2D.simulated = false;
         animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 
 }
